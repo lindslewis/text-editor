c9dc2cf574c095b89a88a8d946511e1bc27c6afe
LindsayLew <lmschwehr@gmail.com>
2022-09-05 09:38:07 -0700

    setting up auth

---
+++ server/middleware/auth.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+// const { ApolloServer, gql } = require('apollo-server-express');
+
+// no no need the jwt first
+
+const jwt = require('jsonwebtoken');
+
+const secret = "this is the secret, bro";
+const expire = '1h';
