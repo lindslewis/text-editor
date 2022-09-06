384366ee9fc8477cc9a27619d77af4a0b7e1bb96
LindsayLew <lmschwehr@gmail.com>
2022-09-05 21:21:53 -0700

    maybe I did the name wrong in the src sw, instead of  matching to pkg json match to database?

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,8 +15,12 @@ const initdb = async () =>
 // Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
   const jateDB = await openDB('jate', 1);
-  
+  const text = jateDB.transaction('jate', 'readwrite');
+  const save = text.objectStore('jate');
+  const request = save.put({ id: 1, value: content});
+  const result = await request;
+  console.log(`${result}`);
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
-  
+
 initdb();
