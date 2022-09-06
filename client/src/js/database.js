8745fc3b4fb45617cdea12004293cd12140a5a96
LindsayLew <lmschwehr@gmail.com>
2022-09-05 21:24:37 -0700

    fixed functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,20 +13,24 @@ const initdb = async () =>
   });
 
 // Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
   const jateDB = await openDB('jate', 1);
   const text = jateDB.transaction('jate', 'readwrite');
   const save = text.objectStore('jate');
   const request = save.put({ id: 1, value: content});
   const result = await request;
   console.log(`${result}`);
+}
+
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
   const jateDB = await openDB('jate', 1);
   const text = jateDB.transaction('jate', 'readonly');
   const save = text.objectStore('jate');
   const request = save.getAll(1);
   const result = await request;
   console.log(`${result}`);
+}
+
   
 initdb();
